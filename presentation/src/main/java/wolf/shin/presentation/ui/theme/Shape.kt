5d1c3ec6df1cf015027caaf2c1a3d07648a8b2ec
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-10 00:29:06 +0900

    feat: domain layer 추가

--- presentation/src/main/java/wolf/shin/presentation/ui/theme/Shape.kt
+++ presentation/src/main/java/wolf/shin/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package wolf.shin.androidearlyadopter.ui.theme
+package wolf.shin.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
