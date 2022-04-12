76c2323eaf5ddc0fa379a7278a90a5277c2dbb39
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-12 20:15:35 +0900

    feat: home screen add

---
+++ presentation/src/main/java/wolf/shin/presentation/ui/screen/home/HomeScreen.kt
@@ -0,0 +1,25 @@
+package wolf.shin.presentation.ui.screen.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HomeScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+
+        Column(
+            modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Home", style = MaterialTheme.typography.h4, color = Color.Black)
+        }
+    }
+}
