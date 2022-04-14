022824e0e52db6cf9747547b1b32ced3b140efc3
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-14 22:18:00 +0900

    feat: home router 생성

--- presentation/src/main/java/wolf/shin/earlyadopter/navigation/ScreenRouter.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/navigation/ScreenRouter.kt
@@ -1,6 +1,11 @@
 package wolf.shin.earlyadopter.navigation
 
 sealed class ScreenRouter(val route: String) {
-    object HomeScreen : ScreenRouter("home")
+    object Home : ScreenRouter("home")
     object NewsScreen : ScreenRouter("news")
+}
+
+enum class HomeRouter(val route: String) {
+    ROOT("home/"),
+    DETAIL("home/detail"),
 }
