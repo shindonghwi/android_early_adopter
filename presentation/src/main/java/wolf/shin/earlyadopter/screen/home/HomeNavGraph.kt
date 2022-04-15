3ee013952c4e7930aa63f5c147331f90f0a416ee
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-15 23:55:54 +0900

    fix: Home Direction 제작 및 Graph 변경

--- presentation/src/main/java/wolf/shin/earlyadopter/screen/home/HomeNavGraph.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/screen/home/HomeNavGraph.kt
@@ -1,22 +1,24 @@
 package wolf.shin.earlyadopter.screen.home
-
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import wolf.shin.earlyadopter.navigation.HomeRouter
-import wolf.shin.earlyadopter.screen.home.detail.HomeDetail
+
+enum class HomeDirections(val route: String) {
+    ROOT("home/"),
+    DETAIL("home/detail"),
+}
 
 fun NavGraphBuilder.addHomeGraph(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-    composable(HomeRouter.ROOT.route) {
+    composable(HomeDirections.ROOT.route) {
         HomeScreen {
-            navController.navigate(HomeRouter.DETAIL.route)
+            navController.navigate(HomeDirections.DETAIL.route)
         }
     }
-    composable(HomeRouter.DETAIL.route) {
-        HomeDetail(from = HomeRouter.DETAIL.route)
+    composable(HomeDirections.DETAIL.route) {
+        DetailScreen(from = HomeDirections.DETAIL.route)
     }
 }
