c1a40594fa283086ea89cfc8bc68230399c8b1d3
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-15 23:55:01 +0900

    fix: Bottom Navigation 로직 변경

--- presentation/src/main/java/wolf/shin/earlyadopter/navigation/BottomNavigation.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/navigation/BottomNavigation.kt
@@ -1,19 +1,24 @@
 package wolf.shin.earlyadopter.navigation
 
+import android.media.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import wolf.shin.earlyadopter.TabDirections
 import wolf.shin.earlyadopter.model.DockBarItem
 
 @Composable
@@ -22,33 +27,59 @@ fun BottomNavigationBar(
     items: List<DockBarItem>,
     navController: NavHostController
 ) {
-    val backStackEntry = navController.currentBackStackEntryAsState()
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = Color.White,
-        elevation = 5.dp
-    ) {
-        items.forEach { tab ->
-            val selected = tab.route == backStackEntry.value?.destination?.route
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute =
+        navBackStackEntry?.destination?.route ?: TabDirections.HOME
+    val tabs = TabDirections.values().toList()
+    BottomNavigation {
+        tabs.forEach { tab ->
+            val route = tab.route
             BottomNavigationItem(
-                selected = selected,
-                onClick = { navController.navigate(tab.route) },
-                selectedContentColor = Color.DarkGray,
-                unselectedContentColor = Color.LightGray,
                 icon = {
-                    Column {
-                        Icon(
-                            painterResource(id = if (selected) tab.selectedIcon else tab.defaultIcon),
-                            contentDescription = tab.name
-                        )
-                        Text(
-                            text = tab.name,
-                            textAlign = TextAlign.Center,
-                            fontSize = 10.sp
-                        )
+                    Image(
+                        painterResource(tab.icon),
+                        contentDescription = "",
+                    )
+                },
+                label = { Text(text = route, fontSize = 9.sp) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == route,
+                onClick = {
+                    if (currentRoute.toString().startsWith(route)) {
+                        navController.navigate(findTabRootRoute(route)) {
+                            popUpTo(findStartDestination(navController.graph).id)
+                        }
+                    } else if (route != currentRoute) {
+                        navController.navigate(route) {
+                            launchSingleTop = true
+                            restoreState = true
+                            val startDestination = findStartDestination(navController.graph)
+                            popUpTo(startDestination.id) {
+                                saveState = true
+                            }
+                        }
                     }
                 }
             )
         }
+    }
+}
+
+
+private tailrec fun findStartDestination(graph: NavDestination): NavDestination {
+    return if (graph is NavGraph) findStartDestination(graph.startDestination!!) else graph
+}
+
+private val NavGraph.startDestination: NavDestination?
+    get() = findNode(startDestinationId)
+
+private fun findTabRootRoute(tab: String): String {
+    return when (tab) {
+        TabDirections.HOME.route -> HomeDirections.ROOT.route
+        TabDirections.COMPANY.route -> CompanyDirections.ROOT.route
+        TabDirections.NOTIFICATION.route -> NotificationDirections.ROOT.route
+        else -> MoreDirections.ROOT.route
     }
 }
