f97e019fa87ced0983e37eefd3294fca32e9cfde
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-15 23:54:34 +0900

    feat: Company 회사 채용정보 화면 추가

---
+++ presentation/src/main/java/wolf/shin/earlyadopter/screen/company/CompanyDirections.kt
@@ -0,0 +1,40 @@
+package wolf.shin.earlyadopter.screen.company
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import io.beomjo.multinav.ui.detail.DetailScreen
+import wolf.shin.earlyadopter.screen.home.detail.DetailScreen
+
+enum class CompanyDirections(val route: String) {
+    ROOT("company/"),
+    DETAIL("company/detail"),
+    DETAIL2("company/detail2"),
+}
+
+fun NavGraphBuilder.addCompanyGraph(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    composable(CompanyDirections.ROOT.route) {
+        CompanyScreen {
+            navController.navigate(CompanyDirections.DETAIL.route)
+        }
+    }
+    composable(CompanyDirections.DETAIL.route) {
+        DetailScreen(from = CompanyDirections.DETAIL.route) {
+            Button(onClick = {
+                navController.navigate(CompanyDirections.DETAIL2.route)
+            }) {
+                Text("Move To Detail 2")
+            }
+        }
+    }
+
+    composable(CompanyDirections.DETAIL2.route) {
+        DetailScreen(from = CompanyDirections.DETAIL.route + "2")
+    }
+}
