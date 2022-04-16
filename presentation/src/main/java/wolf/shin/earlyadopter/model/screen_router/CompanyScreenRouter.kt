670216c1b631e269ca57c05715ac9ebe31736059
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-16 10:46:23 +0900

    refactor: 각 탭에 해당하는 Screen Router 제작 및 Proguard 적용을 위해 model 패키지로 이동

---
+++ presentation/src/main/java/wolf/shin/earlyadopter/model/screen_router/CompanyScreenRouter.kt
@@ -0,0 +1,7 @@
+package wolf.shin.earlyadopter.model.screen_router
+
+enum class CompanyScreenRouter(val route: String) {
+    ROOT("company/"),
+    DETAIL("company/detail"),
+    DETAIL2("company/detail2"),
+}
