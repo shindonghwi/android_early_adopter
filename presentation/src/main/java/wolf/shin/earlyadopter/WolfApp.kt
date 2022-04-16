6d30d6cf45a7b79dcfb1656eaf77714cdb373f46
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-16 10:38:30 +0900

    refactor: 와이어프레임 프리뷰 파일 위치변경

--- presentation/src/main/java/wolf/shin/earlyadopter/WolfApp.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/WolfApp.kt
@@ -3,29 +3,17 @@ package wolf.shin.earlyadopter
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
-import wolf.shin.earlyadopter.model.DockBarItem
 import wolf.shin.earlyadopter.navigation.BottomNavigationBar
-import wolf.shin.earlyadopter.navigation.InitNavigator
 import wolf.shin.earlyadopter.screen.theme.AndroidEarlyAdopterTheme
-import wolf.shin.resource.R
 
 @Composable
 fun WolfApp() {
     AndroidEarlyAdopterTheme {
         val navController = rememberNavController()
-
         Scaffold(
-            bottomBar = { MyBottomNavigation(navController = navController) }
+            bottomBar = { BottomNavigationBar(navController = navController) }
         ) {
-            MyAppNavGraph(navController = navController)
+            WolfAppNavGraph(navController = navController)
         }
     }
-}
-
-enum class TabDirections(
-    val route: String,
-) {
-    HOME("home"),
-    COMPANY("news"),
-
 }
