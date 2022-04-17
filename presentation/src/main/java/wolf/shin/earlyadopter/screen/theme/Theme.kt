501f09ec063aca20db5abf67a32738be21b9ef2a
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-17 21:41:03 +0900

    feat: 기본 앱 테마의 typography 를 pretendard 폰트로 변경

--- presentation/src/main/java/wolf/shin/earlyadopter/screen/theme/Theme.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/screen/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import wolf.shin.earlyadopter.screen.theme.font.PretendardTypography
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -16,15 +17,6 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -37,7 +29,7 @@ fun AndroidEarlyAdopterTheme(darkTheme: Boolean = isSystemInDarkTheme(), content
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography = PretendardTypography,
         shapes = Shapes,
         content = content
     )
