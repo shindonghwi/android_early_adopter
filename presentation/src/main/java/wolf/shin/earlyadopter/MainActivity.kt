a28498ca4bad2f23fc073f299caeccd83b5e7278
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-18 23:32:49 +0900

    feat: Activity OnCreate전에 스플래시 화면 설치 코드 추가

--- presentation/src/main/java/wolf/shin/earlyadopter/MainActivity.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/MainActivity.kt
@@ -3,10 +3,12 @@ package wolf.shin.earlyadopter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import wolf.shin.earlyadopter.screen.theme.AndroidEarlyAdopterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             AndroidEarlyAdopterTheme {
