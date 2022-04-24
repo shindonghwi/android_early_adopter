e19e15a29834bbf859691a290a2fbdbcf2544e5f
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-24 13:55:27 +0900

    feat: 활동명입력 필드 생성 & FadeInOut 애니매이션 적용

--- presentation/src/main/java/wolf/shin/earlyadopter/screen/account/AccountScreen.kt
+++ presentation/src/main/java/wolf/shin/earlyadopter/screen/account/AccountScreen.kt
@@ -7,9 +7,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import wolf.shin.earlyadopter.component.animation.fadeInOutEffectAnimation
 import wolf.shin.earlyadopter.component.image.ProfileImage
-import wolf.shin.earlyadopter.screen.theme.Gray800
+import wolf.shin.earlyadopter.screen.theme.Gray600
+import wolf.shin.resource.R
 
 @Composable
 fun AccountScreen(navigateToDetail: () -> Unit) {
@@ -23,19 +27,24 @@ fun AccountScreen(navigateToDetail: () -> Unit) {
 
 @Composable
 private fun AccountInfo() {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 20.dp), verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 20.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
         ProfileImage(
             modifier = Modifier.size(80.dp),
             imageUrl = "https://windowsforum.kr/files/attach/images/2966154/714/640/006/55e7d5ed1511affba0678ee977c7f40b.jpg"
         )
 
         Text(
-            modifier = Modifier.padding(start = 12.dp),
-            text = "활동명이 필요해요!",
-            style = MaterialTheme.typography.subtitle2,
-            color = Gray800
+            modifier = Modifier
+                .padding(start = 12.dp)
+                .alpha(fadeInOutEffectAnimation(minAlpha = 0.1f, maxAlpha = 1f, duration = 1500)),
+            text = stringResource(id = R.string.accountRoot_title),
+            style = MaterialTheme.typography.subtitle1,
+            color = Gray600
         )
+
     }
 }
